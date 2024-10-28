7bc07c2b84d833c2f7194d0d0bf8451820379e3c
YMM <y7895809@163.com>
2024-10-28 10:50:35 +0000

    input validation

--- test_times.py
+++ test_times.py
@@ -1,5 +1,5 @@
 from times import time_range, compute_overlap_time
-
+import pytest
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -30,8 +30,16 @@ def test_same_time():
     expected = []
     assert result == expected
 
+def input_error():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 09:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 12:45:00")
+    
+    with pytest.raises(ValueError, match="End time cannot earlier than start time"):
+        time_range(large, short)
+
 if __name__ == "__main__":
     #test_given_input()
     #do_not_overlap()
     #both_contain_intervals()
-    test_same_time()
+    #test_same_time()
+    input_error()
